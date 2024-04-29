449215ee668c9475e0183eb46a56f63616e820ba
Shinnosuke1981 <164172955+Shinnosuke1981@users.noreply.github.com>
2024-04-29 18:29:03 +0900

    バリデーション実施

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :height, presence: true
+  validates :email, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: '半角英数字混合を使用してください' }
 end
