d82202ae5b0d9dfd9dccce5ea9c4e0b6670fd236
Shinnosuke1981 <164172955+Shinnosuke1981@users.noreply.github.com>
2024-05-01 17:53:52 +0900

    ヘッダー追加実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,11 @@ class User < ApplicationRecord
   validates :email, presence: true
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: '半角英数字混合を使用してください' }
   validates_inclusion_of :biological_sex_id, in: 2..4, message: 'を選択してください'
+
+  validate :password_include_at
+  def password_include_at
+    unless password.include?("@")
+      errors.add(:password, 'must include @')
+    end
+  end
 end
