f238ebf1bd6fd85507f8e1eff9eec0bc36f971f0
Shinnosuke1981 <164172955+Shinnosuke1981@users.noreply.github.com>
2024-05-01 14:43:22 +0900

    ヘッダー基礎完成

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,7 +1,7 @@
 class DiariesController < ApplicationController
-  before_action :authenticate_user!
+  #before_action :authenticate_user!
 
   def index
-    @diaries = current_user.diaries 
+   # @diaries = current_user.diaries 
   end
 end
