5b202404580fb54930c94eb6bdc9515753426d61
Shinnosuke1981 <164172955+Shinnosuke1981@users.noreply.github.com>
2024-05-01 21:43:39 +0900

    新規登録テスト成功

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,90 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    context '新規登録がうまくいくとき' do
+      it 'nicknameとemail、passwordとpassword_confirmation,  biological_sex_idが存在すれば登録できる' do
+        expect(@user).to be_valid
+      end
+
+      it 'passwordが6文字以上であれば登録できる' do
+        @user.password = 'abc123'
+        @user.password_confirmation = 'abc123'
+        expect(@user).to be_valid
+      end
+
+      it 'biological_sex_idが2以上4以下であれば登録できる' do
+        @user.biological_sex_id = 2
+        expect(@user).to be_valid
+      end
+
+      it 'emailに@が含まれていれば登録できる' do
+        @user.email = 'test@example.com'
+        expect(@user).to be_valid
+      end
+    end
+
+    context '新規登録がうまくいかないとき' do
+      it 'nicknameが空だと登録できない' do
+        @user.nickname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+
+      it 'emailが空では登録できない' do
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+      end
+
+      it 'passwordが空だと登録できない' do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+
+      it 'passwordが5文字以下であれば登録できない' do
+        @user.password = 'abc12'
+        @user.password_confirmation = 'abc12'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+      end
+
+      it 'passwordが存在してもpassword_confirmationが空では登録できない' do
+        @user.password_confirmation = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+
+      it '重複したemailが存在する場合登録できない' do
+        @user.save
+        another_user = FactoryBot.build(:user)
+        another_user.email = @user.email
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include('Email has already been taken')
+      end
+
+      it 'biological_sex_idが1だと登録できない' do
+        @user.biological_sex_id = 1
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Biological sex を選択してください")
+      end
+      
+      it 'biological_sex_idが5以上だと登録できない' do
+        @user.biological_sex_id = 5
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Biological sex を選択してください")
+      end
+
+      it 'emailに@が含まれていないと登録できない' do
+        @user.email = 'testexample.com'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Email must include @')
+      end
+    end
+  end
+end
