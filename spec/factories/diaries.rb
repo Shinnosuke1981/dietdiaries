0364e1747d402e114304b054d64ce1c422dc69f4
Shinnosuke1981 <164172955+Shinnosuke1981@users.noreply.github.com>
2024-05-03 12:20:43 +0900

    日記投稿・単体テスト済

--- spec/factories/diaries.rb
+++ spec/factories/diaries.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :diary do
-    
+    entry_date { Date.today }
+    current_weight { 70 }
+    weight_difference { 1.5 }
+    plus_calories { 1000 }
+    minus_calories { 800 }
+    calorie_balance { 200 }
+    current_BFP { 20 }
+    association :user
   end
 end
--- spec/models/diary_spec.rb
+++ spec/models/diary_spec.rb
@@ -1,7 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Diary, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @diary = FactoryBot.build(:diary)
+  end
+
+  describe 'diary validation' do
+    it 'is valid with valid attributes' do
+      expect(@diary).to be_valid
+    end
+
+    it 'is not valid without an entry_date' do
+      @diary.entry_date = nil
+      expect(@diary).to_not be_valid
+    end
+
+    it 'is not valid without a current_weight' do
+      @diary.current_weight = nil
+      expect(@diary).to_not be_valid
+    end
+
+    it 'is not valid if current_weight is not a number' do
+      @diary.current_weight = 'abc'
+      expect(@diary).to_not be_valid
+    end
+
+    it 'is not valid if current_weight is less than or equal to 0' do
+      @diary.current_weight = 0
+      expect(@diary).to_not be_valid
+    end
+
+    # You can add similar tests for the other attributes too
+  end
 end
-
-
