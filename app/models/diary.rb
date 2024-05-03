0e6edaa314127b59957b46cf649eaab2058427a4
Shinnosuke1981 <164172955+Shinnosuke1981@users.noreply.github.com>
2024-05-03 12:17:24 +0900

    日記投稿バリデーション作成

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,4 +1,11 @@
 class Diary < ApplicationRecord
   belongs_to :user
-  
+
+  validates :entry_date, presence: true
+  validates :current_weight, presence: true, numericality: { greater_than: 0 }
+  validates :weight_difference, numericality: true, allow_blank: true
+  validates :plus_calories, numericality: true, allow_blank: true
+  validates :minus_calories, numericality: true, allow_blank: true
+  validates :calorie_balance, numericality: true, allow_blank: true
+  validates :current_BFP, numericality: true, allow_blank: true
 end
