c728a99e62cb9d8b3431e4e9a4e30acbb1d971bf
Shinnosuke1981 <164172955+Shinnosuke1981@users.noreply.github.com>
2024-05-04 17:30:34 +0900

    カレンダーの可読性改善

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -5,7 +5,7 @@ class Diary < ApplicationRecord
     self.entry_date
   end
 
-  validates :entry_date, presence: true
+  validates :entry_date, presence: true, uniqueness: { scope: :user_id, message: "一日に一つだけ日記を投稿できます" }
   validates :current_weight, presence: true, numericality: { greater_than: 0 }
   validates :weight_difference, numericality: true, allow_blank: true
   validates :plus_calories, numericality: true, allow_blank: true
--- spec/models/diary_spec.rb
+++ spec/models/diary_spec.rb
@@ -5,31 +5,40 @@ RSpec.describe Diary, type: :model do
     @diary = FactoryBot.build(:diary)
   end
 
-  describe 'diary validation' do
-    it 'is valid with valid attributes' do
-      expect(@diary).to be_valid
+  describe '日記投稿' do
+    context '日記投稿ができるとき' do
+      it '全ての値が正しければ投稿できる' do
+        expect(@diary).to be_valid
+      end
     end
 
-    it 'is not valid without an entry_date' do
-      @diary.entry_date = nil
-      expect(@diary).to_not be_valid
-    end
+    context '日記投稿ができないとき' do
+      it '日付がないと投稿できない' do
+        @diary.entry_date = nil
+        expect(@diary).to_not be_valid
+      end
 
-    it 'is not valid without a current_weight' do
-      @diary.current_weight = nil
-      expect(@diary).to_not be_valid
-    end
+      it '同じ日に複数回投稿できない' do
+        user = FactoryBot.create(:user)
+        first_diary = FactoryBot.create(:diary, user: user, entry_date: Date.today)
+        duplicate_diary = FactoryBot.build(:diary, user: user, entry_date: Date.today)
+        expect(duplicate_diary).to_not be_valid
+      end
 
-    it 'is not valid if current_weight is not a number' do
-      @diary.current_weight = 'abc'
-      expect(@diary).to_not be_valid
-    end
+      it '体重がないと投稿できない' do
+        @diary.current_weight = nil
+        expect(@diary).to_not be_valid
+      end
 
-    it 'is not valid if current_weight is less than or equal to 0' do
-      @diary.current_weight = 0
-      expect(@diary).to_not be_valid
-    end
+      it '体重が数字でないと投稿できない' do
+        @diary.current_weight = 'abc'
+        expect(@diary).to_not be_valid
+      end
 
-    # You can add similar tests for the other attributes too
+      it '体重が0以下だと投稿できない' do
+        @diary.current_weight = 0
+        expect(@diary).to_not be_valid
+      end
+    end
   end
 end
