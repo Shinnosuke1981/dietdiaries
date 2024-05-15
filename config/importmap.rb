025961e10ab2ff108bf92a3d74e0fa03ecd0ca38
Shinnosuke1981 <164172955+Shinnosuke1981@users.noreply.github.com>
2024-05-15 18:19:10 +0900

    自動計算を本番で実行

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,4 +6,4 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "chartkick", to: "chartkick.js"
-pin "chart.js", to: "chart.js/dist/Chart.js"
+pin "chart.js" # @4.4.2pin "@kurkle/color", to: "@kurkle--color.js" # @0.3.2
